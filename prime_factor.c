c95995c40e996a50efee9fe28747c056571f7f99
Drihmia <drihmia.redouane@gmail.com>
2023-11-19 11:49:57 +0100

    print also the result if the number is divisible by non prime numbers

--- prime_factor.c
+++ prime_factor.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdint.h>
 #include <gmp.h>
 
-#define ARRAY_SIZE 2
 
 void prime_nums_of(mpz_t n);
 /**
@@ -32,7 +30,7 @@ int main(int ac, char **av)
 		perror("Error opening file");
 		return (1);
 	}
-
+	/* initiliaze mpz_t variable */
 	mpz_init(num_elements);
 
 	while (getline(&line, &size, file) != -1)
@@ -47,11 +45,11 @@ int main(int ac, char **av)
 	return (0);
 }
 /**
-  * prime_nums_of - giving the prime numbers of number n.
-  * this function gives  prime numbers fron 2 to the given n.
-  * @n: the target number.
-  * Return: None.
-  */
+ * prime_nums_of - giving the prime numbers of number n.
+ * this function gives  prime numbers fron 2 to the given n.
+ * @n: the target number.
+ * Return: None.
+ */
 void prime_nums_of(mpz_t n)
 {
 	int l, k, x, get_i;
@@ -67,40 +65,24 @@ void prime_nums_of(mpz_t n)
 	}
 
 	mpz_t i, j, sqrt_n, m;
-	mpz_init(i);
-	mpz_init(j);
-	mpz_init(m);
-	mpz_init(sqrt_n);
-	mpz_sqrt(sqrt_n, n);
 
-	for (mpz_init_set_ui(i, 2); mpz_cmp(i, sqrt_n) <= 0; mpz_add_ui(i, i, 1))
+	mpz_init(i);
+	mpz_init2(m, 128000);
+
+	for (mpz_init_set_ui(i, 2); mpz_cmp(i, n) <= 0; mpz_add_ui(i, i, 1))
 	{
-		l = 0;
 		/* check how many times n is divisible by i */
 		if (mpz_divisible_p(n, i) != 0)
 		{
-			mpz_set(j, i);
-			get_i = mpz_get_si(i);
-			while (mpz_cmp_ui(j, 0) > 0)
-			{
-				if (mpz_divisible_p(i, j) != 0)
-					l += 1;
-				mpz_sub_ui(j, j, 1);
-
-				if (l == 2)
-				{
-					mpz_tdiv_q_ui(m, n, get_i);
-					gmp_printf("%Zd=%Zd*%Zd\n", n, m, i);
-					break;
-				}
-			}
-			if (l == 2)
-				break;
+			get_i = mpz_get_ui(i);
+			mpz_divexact(m, n, i);
+			gmp_printf("%Zd=%Zd*%Zd\n", n, m, i);
+			break;
 		}
+		gmp_printf("%Zd\n", i);
 	}
 	mpz_clear(i);
-	mpz_clear(j);
 	mpz_clear(m);
-	mpz_clear(sqrt_n);
 }
+
 
