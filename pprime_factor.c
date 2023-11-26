f7a9d6995a4447f7f1ab145437544b4b68736e5b
Drihmia <drihmia.redouane@gmail.com>
2023-11-26 11:48:48 -0800

    improvement1

--- pprime_factor.c
+++ pprime_factor.c
@@ -3,31 +3,40 @@
 #include <gmp.h>
 
 /**
- * faize_number - Factorize the given number and print the result.
+ * factorize_number - Factorize the given number and print the result.
  *
- * @n: The number to faize.
+ * @n: The number to factorize.
  */
-void faize_number(mpz_t n)
+void factorize_number(mpz_t n)
 {
-	mpz_t fa, m;
+	mpz_t fa, qu, sqrt_n;
 
-	if (mpz_cmp_ui(n, 1) == 0 || mpz_cmp_ui(n, 2) == 0 || mpz_cmp_ui(n, 3) == 0)
+	mpz_init(fa), mpz_init(qu), mpz_init(sqrt_n), mpz_sqrt(sqrt_n, n);
+	if (mpz_divisible_ui_p(n, 2))
 	{
-		gmp_printf("%Zd=%Zd*1\n", n, n);
+		mpz_divexact_ui(qu, n, 2);
+		gmp_printf("%Zd=%Zd*2\n", n, qu), mpz_clear(fa);
+		mpz_clear(qu), mpz_clear(sqrt_n);
 		return;
 	}
 
-	mpz_init(fa);
-	mpz_init(m);
+	if (mpz_divisible_ui_p(n, 3))
+	{
+		mpz_divexact_ui(qu, n, 3);
+		gmp_printf("%Zd=%Zd*3\n", n, qu), mpz_clear(fa);
+		mpz_clear(qu), mpz_clear(sqrt_n);
+		return;
+	}
 
-	for (mpz_set_ui(fa, 2); mpz_cmp(fa, n) <= 0; mpz_nextprime(fa, fa))
+	for (mpz_set_ui(fa, 5); mpz_cmp(fa, sqrt_n) <= 0; mpz_nextprime(fa, fa))
 	{
 		if (mpz_divisible_p(n, fa))
 		{
-			mpz_divexact(m, n, fa);
-			gmp_printf("%Zd=%Zd*%Zd\n", n, m, fa);
+			mpz_divexact(qu, n, fa);
+			gmp_printf("%Zd=%Zd*%Zd\n", n, qu, fa);
 			mpz_clear(fa);
-			mpz_clear(m);
+			mpz_clear(qu);
+			mpz_clear(sqrt_n);
 			return;
 		}
 	}
@@ -35,17 +44,16 @@ void faize_number(mpz_t n)
 	gmp_printf("%Zd=%Zd*1\n", n, n);
 
 	mpz_clear(fa);
-	mpz_clear(m);
+	mpz_clear(qu);
+	mpz_clear(sqrt_n);
 }
-
 /**
  * main - Main function to read numbers from a file and factorize them.
  *
  * @argc: Number of command-line arguments.
  * @argv: Command-line arguments.
- * Return: 0 on success, 1 otherwise
+ * Return: 0 on success, 1 otherwise.
  */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -65,12 +73,11 @@ int main(int argc, char *argv[])
 	char buffer[10000];
 	mpz_t number;
 
-
 	while (fgets(buffer, sizeof(buffer), file) != NULL)
 	{
 		mpz_init(number);
 		mpz_set_str(number, buffer, 10);
-		faize_number(number);
+		factorize_number(number);
 		mpz_clear(number);
 	}
 
